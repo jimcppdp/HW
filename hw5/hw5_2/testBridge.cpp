ab843617d26789d2f576cf8b0e34a599a159108b
jimcppdp <jimcppdp@gmail.com>
2017-05-12 08:21:17 +0000

    Added HW 5-2, 5-3

---
+++ hw5/hw5_2/testBridge.cpp
@@ -0,0 +1,70 @@
+#include <string>
+#include <iostream>
+#include <memory>
+#include <map>
+#include "gtest/gtest.h"
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+class DrawApi
+{
+  public:
+    virtual std::string draw() = 0;
+};
+
+class OpenGL_api: public DrawApi
+{
+  public:
+    std::string draw() { return "using OpenGL to draw"; }
+};
+    
+class DirectX_api: public DrawApi
+{
+  public:
+    std::string draw() { return "using DirectX to draw"; }
+};
+
+class Shape
+{
+  public:
+    Shape(DrawApi* dapi) { mDrawApi = dapi; }
+    virtual std::string draw() = 0;
+
+  protected:
+    DrawApi* mDrawApi;
+};
+
+class Triangle: public Shape
+{
+  public:
+    Triangle(DrawApi* dapi): Shape(dapi) {}
+
+    std::string draw() { return mDrawApi->draw(); }
+};
+
+class Circle: public Shape
+{
+  public:
+    Circle(DrawApi* dapi): Shape(dapi) {}
+
+    std::string draw() { return mDrawApi->draw(); }
+};
+
+    
+
+TEST(testBridge, BridgeShapeDraw)
+{
+  Shape* triangle = new Triangle( new OpenGL_api );
+  Shape* circle = new Circle( new DirectX_api );
+  EXPECT_EQ(triangle->draw(), "using OpenGL to draw");
+  EXPECT_EQ(circle->draw(), "using DirectX to draw");
+  delete triangle;
+  delete circle;
+}
+
+
+
---
+++ hw5/hw5_3/testAdapter.cpp
@@ -0,0 +1,89 @@
+#include <string>
+#include <iostream>
+#include "gtest/gtest.h"
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+class DrawApi
+{
+  public:
+    virtual std::string draw() = 0;
+};
+
+class OpenGL_api: public DrawApi
+{
+  public:
+    std::string draw() { return "using OpenGL to draw"; }
+};
+    
+class DirectX_api: public DrawApi
+{
+  public:
+    std::string draw() { return "using DirectX to draw"; }
+};
+
+class Shape
+{
+  public:
+    Shape(DrawApi* dapi) { mDrawApi = dapi; }
+    virtual std::string draw() = 0;
+    friend class AdapterTriangle;
+
+  protected:
+    DrawApi* mDrawApi;
+};
+
+class Triangle: public Shape
+{
+  public:
+    Triangle(DrawApi* dapi): Shape(dapi)
+    {
+    }
+
+    std::string draw() { return mDrawApi->draw(); }
+};
+
+class Circle: public Shape
+{
+  public:
+    Circle(DrawApi* dapi): Shape(dapi) {}
+
+    std::string draw() { return mDrawApi->draw(); }
+};
+
+
+class NewShape
+{
+  public:
+    virtual std::string newdraw() = 0;
+
+};
+
+class AdapterTriangle: public NewShape, private Triangle
+{
+  public:
+
+    AdapterTriangle(Triangle* tri): Triangle(tri->mDrawApi) 
+    { 
+      mTriangle = tri;
+    }
+    std::string newdraw()
+    {
+       return mTriangle->draw();
+    }
+
+  private:
+    Triangle* mTriangle;
+};
+
+TEST(testAdapter, AdapterNewDraw)
+{
+  NewShape* newtriangle = new AdapterTriangle( new Triangle ( new OpenGL_api) );
+  EXPECT_EQ(newtriangle->newdraw(), "using OpenGL to draw");
+  delete newtriangle;
+}
+
