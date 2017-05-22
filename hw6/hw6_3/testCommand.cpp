fbe642d420c7b2b020543e0fac64fd9c3a20afc7
jimcppdp <jimcppdp@gmail.com>
2017-05-22 17:44:16 +0800

    Added HW 6-3

---
+++ hw6/hw6_3/testCommand.cpp
@@ -0,0 +1,72 @@
+#include <string>
+#include <iostream>
+#include <memory>
+#include <map>
+#include "gtest/gtest.h"
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+
+class Command {
+  public:
+    virtual void execute() = 0;
+};
+
+class SimpleCommand : public Command
+{
+  public:
+  std::function<void(int*)> _cb; // callback
+  int* receiver;  // has_a instance
+
+  SimpleCommand(int* rec, std::function<void(int*)> cb)
+      : receiver(rec), _cb(cb)
+  { }
+
+  virtual void execute()
+  {
+    //std::cout << "SimpleCommand::execute() is called" << "\n";
+    //std::cout << "SimpleCommand::receiver:" << *receiver << "\n";
+    _cb(receiver);
+  }
+  
+};
+
+void divide(int var);
+void add(int var);
+
+int variableA (10);
+int variableB (5);
+TEST(testCommand, UseCase1)
+{
+  //int variableA (10);
+  //int variableB (5);
+  std::vector<Command*> commands;
+  commands.emplace_back (new SimpleCommand(&variableA, std::bind (&divide, variableB)) );
+  commands.emplace_back (new SimpleCommand(&variableA, std::bind (&add, variableB)) );
+  EXPECT_EQ (variableA, 10);
+
+  for (auto& c : commands)
+    c->execute();
+
+  EXPECT_EQ (variableA, 7);
+}
+
+
+void divide(int var)
+{
+  //std::cout << "divide() is called" << "\n";
+  //std::cout << "var:" << var << "\n";
+  variableA =  variableA / var;
+}
+
+void add(int var)
+{
+  //std::cout << "add() is called" << "\n";
+  //std::cout << "var:" << var << "\n";
+  variableA =  variableA + var;
+}
+
