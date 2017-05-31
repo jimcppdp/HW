eea4d3cde0dbc72e29634fe38c29b3a89fafee2d
jimcppdp <jimcppdp@gmail.com>
2017-05-31 06:41:09 +0000

    Code clean up

--- hw7/hw7_2/testComposite.cpp
+++ hw7/hw7_2/testComposite.cpp
@@ -13,53 +13,6 @@ int main(int argc, char* argv[])
     return RUN_ALL_TESTS();
 }
 
-/*
-
-vector<composite*> nodes(10);
-Nodes[0] = new container;
-
-Class composite
-{
-  Virtual void traverse() = 0;
-
-};
-
-Class leaf: composite
-{
-  Virtual void traverse()
-  {
-    Cout << “I am a leaf”
-  }
-
-}
-
-Class container: composite
-{
-  Vector <composite*> childs;
-  Void traverse()
-  {
-    Cout << “I am a folder”
-    for(auto& c: childs)
-    {
-      c->traverse()
-    }
-}
-
-main()
-{
-  vector<composite*> all_nodes(4);
-  All_node[0] = new leaf;
-  All_node[1] = new leaf;
-  All_node[2] = new container(all_node);
-   all_node[3]->traverse;
-      // I am a folder
-      // I am a leaf
-}
-
-*/
-
-// make global result to local in class
-//std::string result;
 
 class Employee // composite
 {
@@ -82,7 +35,6 @@ class Worker : public Employee // leaf
     virtual void manage(Employee* e) { }
     
     virtual std::string who() { 
-      //result = result + _name + " ";
       return _name + " "; 
     }
 };
@@ -103,13 +55,10 @@ class Manager : public Employee // container 1
     }
   
     virtual std::string who() { 
-      //result = result + _name + " ";
       local_result = _name + " ";
       
-      //for_each(eList.begin(), eList.end(), std::mem_fun(&Employee::who));
       for(auto& func: eList)
       {
-        //std::cout << func->who();
         local_result = local_result + func->who();
       }      
       
@@ -134,13 +83,10 @@ class Director : public Employee // container 2
     
     virtual std::string who()
     {
-      //result = _name + " ";
       local_result = _name + " ";
 
-      //for_each(eList.begin(), eList.end(), std::mem_fun(&Employee::who));
       for(auto& func: eList)
       {
-        //std::cout << func->who();
         local_result = local_result + func->who();
       }
 
